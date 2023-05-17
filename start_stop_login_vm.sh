b12ef92f49c3c60cac6d54a9091ff20a6b15a09c
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2023-05-17 17:21:16 +0300

    VM from CLI

--- start_stop_login_vm.sh
+++ start_stop_login_vm.sh
@@ -8,17 +8,22 @@ process=$(virsh list --all | grep -v "Id" | grep -v "-" | awk '{print $3}')
 if [ "$process" ]; then
 	virsh net-dhcp-leases default
 	echo
-	read -p "Do you want to login? (type 'yes') " login_choice
+	echo "Do you want to log in to a running VM or shutdown a running VM?"
+       	read -p "Type 'yes' to log in, 'no' to skip logging in or 'stop' to shutdown VM: " login_choice
 	echo
 	if [ "$login_choice" = 'yes' ]; then
 		read -p "Enter your username: " usr
 		read -p "Enter the IP: " ip
 		/usr/bin/ssh "$usr"@"$ip"
-	else
+	elif [ "$login_choice" = 'no' ]; then
 		echo "VM is running and ready to login."
+	elif [ "$login_choice" = 'stop' ]; then
+		read -p "Enter the name of the VM you want to shutdown: " vm_stop
+		/usr/bin/virsh shutdown "$vm_stop"
 	fi
 else
-	read -p "Do you want to start/stop any VM? (type 'start', 'stop' or 'no action'): " choice
+	echo "Do you want to start any VM?" 
+	read -p "Type 'start' to start the VM or 'no action': " choice
 	echo
 	if [ "$choice" = 'start' ]; then
 		read -p "Enter name of the VM you want to start: " vm_start
@@ -29,23 +34,21 @@ else
 		echo
 		virsh net-dhcp-leases default
 		echo
-		read -p "Do you want to login (type 'yes' to login): " login
+		echo "Do you want to login?" 
+		read -p "Type 'yes' to log in, 'no' skip logging in: " login
 		echo
 		if [ "$login" = 'yes' ]; then
 			read -p "Enter your username: " usr
 			read -p "Enter the IP: " ip
 			/usr/bin/ssh "$usr"@"$ip"
 			echo
-		else
+		elif [ "$login" = 'no' ]; then
 			echo "VM is ready to log in."
 		fi
-	elif [ "$choice" = 'stop' ]; then
-		read -p "Enter the name of the VM you want to stop: " vm_stop
-		/usr/bin/virsh shutdown "$vm_stop"
 	elif [ "$choice" = 'no action' ]; then
 		echo "This script is for starting/stoping VM from CLI."
 	else
 		echo
-		echo "Wrong input!! Please type 'start' or 'stop'."
+		echo "Wrong input!!"
 	fi
 fi
